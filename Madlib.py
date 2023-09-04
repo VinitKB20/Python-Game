b70f92f1cd72dd8e2a5e05a6e9998ac43bdc59bf
Vinit Belanke <vinit.belanke20@gmail.com>
2023-09-04 16:18:31 +0530

    inital commit

---
+++ Madlib.py
@@ -0,0 +1,7 @@
+name = input("What is your name: ")
+colour = input("What is your favourite colour: ")
+celebrity = input("What is celebrity name: ")
+print("Your name is "+ name )
+print("Vinit's Favorite colour is "+ colour)
+print(celebrity + " is the world's famous celebrity")
+
